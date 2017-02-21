f8aefc7dc603d0b0a0e82f72e10947214aae6d58
Lukas Hechenberger <admin@ls-age.com>
2017-02-21 14:55:48 +0100

    Use gulp-util for logging

--- src/bin/automated-release.js
+++ src/bin/automated-release.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import colors from 'chalk';
+import { log } from 'gulp-util';
 import AutomatedRelease from '../index';
 
 (new AutomatedRelease(process.argv.slice(2)))
   .launch()
   .catch(err => {
-    console.error(colors.red(`${err.constructor.name}: ${err.message}`));
+    log.warn(log.colors.red(`${err.constructor.name}: ${err.message}`));
     process.exit(1);
   });
