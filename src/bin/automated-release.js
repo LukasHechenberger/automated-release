3944fde8972ef6dd8f1c98dbd2bc4d3c8ae331ba
Lukas Hechenberger <admin@ls-age.com>
2017-02-21 15:03:10 +0100

    Fix typo

--- src/bin/automated-release.js
+++ src/bin/automated-release.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import colors from 'chalk';
-import { log } from 'gulp-util';
+
+import { log, colors } from 'gulp-util';
 import AutomatedRelease from '../index';
 
 (new AutomatedRelease(process.argv.slice(2)))
   .launch()
   .catch(err => {
-    log.warn(log.colors.red(`${err.constructor.name}: ${err.message}`));
+    log.warn(colors.red(`${err.constructor.name}: ${err.message}`));
     process.exit(1);
   });
