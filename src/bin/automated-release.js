f9b45d5ba16ebfb43b846d6302f2bfc4db95e519
Lukas Hechenberger <admin@ls-age.com>
2017-02-21 13:39:37 +0100

    Color error message

--- src/bin/automated-release.js
+++ src/bin/automated-release.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+import colors from 'chalk';
 import AutomatedRelease from '../index';
 
 (new AutomatedRelease(process.argv.slice(2)))
   .launch()
   .catch(err => {
-    console.error(err);
+    console.error(colors.red(`${err.constructor.name}: ${err.message}`));
     process.exit(1);
   });
