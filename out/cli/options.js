deabef751a5db4ac23ef07d3f2ca93a5a5d1c376
Lukas Hechenberger <admin@ls-age.com>
2017-02-22 12:59:40 +0100

    Version 0.1.12 for distribution [ci skip]

---
+++ out/cli/options.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  'add-files': {
+    alias: 'f',
+    desc: 'Add files to release',
+    type: 'array'
+  },
+  'github-token': {
+    alias: 't',
+    desc: 'An oauth token to create releases on github.com',
+    type: 'string'
+  }
+};
